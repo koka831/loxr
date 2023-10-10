72ff3dc9da32cef1f641e2eb59c32910a8b94444
koka <koka.code@gmail.com>
2023-10-11 00:27:39 +0900

    wip: consolidate expression syntax tree

--- src/ast.rs
+++ src/ast.rs
@@ -4,9 +4,6 @@
 //! CAPITALIZE -- terminal symbol; literal.
 //! x -> a ;   -- definition of x (non-terminal)
 //!
-//! literal     -> NUMBER | STRING | "true" | "false" | "nil" ;
-//! operator    -> "==" | "!=" | "<" | "<=" | ">" | ">=" | "+" | "-" | "*" | "/" ;
-//!
 //! ## Expressions:
 //!
 //! expression  -> equality ;
@@ -16,66 +13,15 @@
 //! factor      -> unary ( ("/" | "*") unary )* ;
 //! unary       -> ( "-" | "!" ) unary | primary ;
 //! primary     -> literal | "(" expression ")" ;
+//! literal     -> NUMBER | STRING | "true" | "false" | "nil" ;
 use std::fmt;
 
 use crate::span::Span;
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum BinOp {
-    /// "=="
-    Equal,
-    /// "!="
-    NotEqual,
-    /// "<"
-    Lt,
-    /// "<="
-    Leq,
-    /// ">"
-    Gt,
-    /// ">="
-    Geq,
-    Plus,
-    Minus,
-    Mul,
-    Div,
-}
-
-impl fmt::Display for BinOp {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use BinOp::*;
-
-        let token = match self {
-            Equal => "==",
-            NotEqual => "!=",
-            Lt => "<",
-            Leq => "<=",
-            Gt => ">",
-            Geq => ">=",
-            Plus => "+",
-            Minus => "-",
-            Mul => "*",
-            Div => "/",
-        };
-
-        write!(f, "{token}")
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
 pub enum UnOp {
     Minus,
     Not,
-}
-
-impl fmt::Display for UnOp {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let token = match self {
-            UnOp::Minus => "-",
-            UnOp::Not => "!",
-        };
-
-        write!(f, "{token}")
-    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -91,6 +37,92 @@ pub enum Literal<'s> {
     True,
     False,
     Nil,
+}
+
+pub enum Primary<'s> {
+    Literal(Literal<'s>),
+    /// grouped expression "(" expr ")"
+    Grouped(Box<Expr<'s>>),
+}
+
+pub enum UnaryKind<'s> {
+    UnOp {
+        op: Option<UnOp>,
+        unary: Box<Unary<'s>>,
+    },
+    Primary(Primary<'s>),
+}
+
+pub struct Unary<'s> {
+    kind: UnaryKind<'s>,
+    span: Span,
+}
+
+pub enum FactorOp {
+    Div,
+    Mul,
+}
+
+pub struct Factor<'s> {
+    lhs: Unary<'s>,
+    rhs: Option<(FactorOp, Unary<'s>)>,
+    pub span: Span,
+}
+
+pub enum TermOp {
+    Plus,
+    Minus,
+}
+
+pub struct Term<'s> {
+    lhs: Factor<'s>,
+    rhs: Option<(TermOp, Factor<'s>)>,
+    pub span: Span,
+}
+
+pub enum Compare {
+    Lt,
+    Leq,
+    Gt,
+    Geq,
+}
+
+pub struct Comparison<'s> {
+    lhs: Term<'s>,
+    rhs: Option<(Compare, Term<'s>)>,
+    pub span: Span,
+}
+
+pub enum EqOp {
+    Eq,
+    Neq,
+}
+
+pub struct Equality<'s> {
+    lhs: Comparison<'s>,
+    rhs: Option<(EqOp, Comparison<'s>)>,
+    pub span: Span,
+}
+
+#[non_exhaustive]
+pub enum ExprKind<'s> {
+    Equality(Equality<'s>),
+}
+
+pub struct Expr<'s> {
+    pub kind: ExprKind<'s>,
+    pub span: Span,
+}
+
+impl fmt::Display for UnOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let token = match self {
+            UnOp::Minus => "-",
+            UnOp::Not => "!",
+        };
+
+        write!(f, "{token}")
+    }
 }
 
 impl<'s> fmt::Display for Literal<'s> {
@@ -112,50 +144,90 @@ impl<'s> fmt::Display for Literal<'s> {
     }
 }
 
-pub struct Binary<'s> {
-    pub lhs: Box<Expr<'s>>,
-    pub op: BinOp,
-    pub rhs: Box<Expr<'s>>,
-}
-
-impl<'s> fmt::Display for Binary<'s> {
+impl<'s> fmt::Display for Primary<'s> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} {} {}", self.lhs, self.op, self.rhs)
+        todo!()
     }
-}
-
-pub struct Unary<'s> {
-    pub op: UnOp,
-    pub rhs: Box<Expr<'s>>,
 }
 
 impl<'s> fmt::Display for Unary<'s> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}{}", self.op, self.rhs)
+        match self.kind {
+            UnaryKind::UnOp { op, unary } => match op {
+                Some(op) => write!(f, "{}{}", op, unary),
+                None => write!(f, "{}", unary),
+            },
+            UnaryKind::Primary(primary) => primary.fmt(f),
+        }
     }
 }
 
-pub enum ExprKind<'s> {
-    Literal(Literal<'s>),
-    BinOp(Binary<'s>),
-    UnOp(Unary<'s>),
-    Group(Box<Expr<'s>>),
+impl fmt::Display for FactorOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let op = match self {
+            FactorOp::Div => "/",
+            FactorOp::Mul => "*",
+        };
+        write!(f, "{op}")
+    }
 }
 
-pub struct Expr<'s> {
-    pub kind: ExprKind<'s>,
-    pub span: Span,
+impl fmt::Display for TermOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let op = match self {
+            TermOp::Plus => "+",
+            TermOp::Minus => "-",
+        };
+        write!(f, "{op}")
+    }
 }
+
+impl fmt::Display for Compare {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Compare::*;
+        let op = match self {
+            Lt => "<",
+            Leq => "<=",
+            Gt => ">",
+            Geq => ">=",
+        };
+        write!(f, "{op}")
+    }
+}
+
+impl fmt::Display for EqOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let op = match self {
+            EqOp::Eq => "==",
+            EqOp::Neq => "!=",
+        };
+        write!(f, "{op}")
+    }
+}
+
+// macro to impl fmt::Display for struct s.t. lhs and optional rhs.
+macro_rules! impl_display {
+    ($strukt:ident) => {
+        impl<'s> ::std::fmt::Display for $strukt<'s> {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                match self.rhs {
+                    None => write!(f, "{}", self.lhs),
+                    Some((op, rhs)) => write!(f, "{} {} {}", self.lhs, op, rhs),
+                }
+            }
+        }
+    };
+}
+
+impl_display!(Factor);
+impl_display!(Term);
+impl_display!(Comparison);
+impl_display!(Equality);
 
 impl<'s> fmt::Display for Expr<'s> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use ExprKind::*;
-
         match self.kind {
-            Literal(ref lit) => lit.fmt(f),
-            BinOp(ref binary) => binary.fmt(f),
-            UnOp(ref unary) => unary.fmt(f),
-            Group(box ref expr) => write!(f, "({})", expr),
+            ExprKind::Equality(ref equality) => equality.fmt(f),
         }
     }
 }
